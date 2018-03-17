5a057ec2bd0522c346177b38f2509ff43b4149f9
octaexon <octaexon@gmail.com>
2018-03-17 22:33:14 +0100

    Add label map creator

---
+++ data/sign_detection_data/create_labelmap.py
@@ -0,0 +1,136 @@
+import argparse
+import os
+import pandas as pd
+
+
+''' create_labelmap.py
+
+    converts image and class metadata stored 
+    in csv format to a label map
+'''
+
+__author__ = 'James Ryan'
+__copyright__ = 'Copyright 2018'
+__credits__ = ['James Ryan']
+__license__ = 'MIT'
+__version__ = '0.1'
+__maintainer__ = 'James Ryan' 
+__email__ = 'octaexon@gmail.com'
+__status__ = 'Development'
+
+
+import metadata_utilities as utils
+
+
+#SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+SCRIPT_DIR = '.'
+CLASS_CSV = os.path.join(SCRIPT_DIR, 'metadata/class_metadata.csv')
+IMAGE_CSV = os.path.join(SCRIPT_DIR, 'metadata/image_metadata.csv')
+DST_DIR = os.path.join(SCRIPT_DIR, 'metadata')
+
+#LABEL_MAP_FILENAME_ALL = 'all_label_map.pbtxt'
+LABEL_MAP_FILENAME_TEMPLATE = 'top_{top}_label_map.pbtxt'
+
+#LABEL_MAP_TEMPLATE = 'item {{\n    id: {id}\n    name: "{description}"\n}}\n\n'
+LABEL_MAP_TEMPLATE = ('item {{\n    id: {label}\n    '
+                      + 'name: "{description}"\n    '
+                      + 'original_id: {class_id}\n    '
+                      + 'category: "{category}"\n}}')
+
+
+def metadata2labelmap(metadata):
+    ''' csv to label map converter
+
+        Parameters
+
+        metadata: pandas.DataFrame
+            metadata must contain columns: label, class_id, description, category
+
+
+        Returns
+
+        label_map: str
+            label map for the "top" classes with fields:
+                id: metadata.label             <- class identifier (*)
+                name: metadata.description     <- class display (*)
+                original_id: metadata.class_id <- original class id in class metadata
+                category: metadata.category    <- category for sign
+
+        Notes
+        (*) signifies that this is a label map field required by 
+            object detection api
+
+        id parameters for label map must be unique and greater than 0, since
+        0 is reserved as the id for the null class
+    '''
+
+    return (metadata[['label', 'description', 'class_id', 'category']]
+                    .drop_duplicates()
+                    .sort_values(by='label')
+                    .apply(lambda x: LABEL_MAP_TEMPLATE.format(**x.to_dict()), axis=1)
+                    .str.cat(sep='\n\n'))
+
+
+
+def process_metadata(class_metadata_path, image_metadata_path, dst_dir, top):
+    ''' ingest metadata, call converter, write label map
+
+        Parameters
+
+        class_metadata_path: str
+            path to csv file
+
+        image_metadata_path: str
+            path to csv file
+
+        dst_dir: str
+            directory name for output
+
+        top: False or int
+            positive int indicating "top" most frequent classes
+            to select
+            False to indicate that all classes should be selected
+    '''
+    metadata = utils.load_metadata(class_metadata_path, image_metadata_path)
+
+    metadata = utils.add_labels(metadata)
+
+    top, metadata = utils.get_top_metadata(top, metadata)
+ 
+    label_map_path = utils.create_output_path(dst_dir, LABEL_MAP_FILENAME_TEMPLATE, top=top)
+
+    with open(label_map_path, 'w') as fd:
+        # create label map parameters
+        label_map = metadata2labelmap(metadata)
+        # write to disk
+        fd.write(label_map)
+
+
+
+
+if __name__ == "__main__":
+    # setup parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--class-metadata', 
+                        dest='class_metadata_path', 
+                        default=CLASS_CSV,
+                        help='Input csv file containing class metadata')
+    parser.add_argument('-i', '--image-metadata', 
+                        dest='image_metadata_path', 
+                        default=IMAGE_CSV,
+                        help='Input csv file containing image metadata')
+    parser.add_argument('-d', '--destination-directory', 
+                        dest='dst_dir', 
+                        type=str, 
+                        default=DST_DIR,
+                        help='Destination directory for label map')
+    parser.add_argument('-t', '--top', 
+                        dest='top', 
+                        default=False, 
+                        type=utils.positive_int,
+                        help='-t 5 means top 5 labels in terms of frequency \
+                              should be included in label map')
+
+    args, _ = parser.parse_known_args()
+
+    process_metadata(**vars(args))
---
+++ data/sign_detection_data/metadata_utilities.py
@@ -0,0 +1,151 @@
+import argparse
+import os
+import pandas as pd
+
+''' metadata_utilities.py
+
+    convenience functions for other modules
+'''
+
+__author__ = 'James Ryan'
+__copyright__ = 'Copyright 2018'
+__credits__ = ['James Ryan']
+__license__ = 'MIT'
+__version__ = '0.1'
+__maintainer__ = 'James Ryan' 
+__email__ = 'octaexon@gmail.com'
+__status__ = 'Development'
+
+
+
+def positive_int(arg_string):
+    ''' converts parsed argument string to integer, checking for positivity
+
+        Parameter
+
+        arg_string: str
+            argument string to be converted and checked
+
+
+        Returns
+
+        positive integer
+
+
+        Raises
+
+        argparse.ArgumentTypeError
+    '''
+    try:
+        # attempt conversion
+        positive_integer = int(arg_string)
+
+        # check for positivity
+        if not positive_integer > 0:
+            raise ValueError
+
+    except ValueError:
+        msg = 'invalid unsigned int value: \'{}\''.format(arg_string)
+        raise argparse.ArgumentTypeError(msg)
+
+    return positive_integer
+
+
+
+def load_metadata(class_metadata_path, image_metadata_path):
+    ''' metadata loader
+
+        Parameters
+
+        class_metadata_path: str
+            required column: class_id
+
+        image_metadata_path: str
+            required column: class_id
+
+
+        Returns
+
+        merged metadata: pandas.DataFrame
+    '''
+    return pd.merge(pd.read_csv(class_metadata_path), 
+                    pd.read_csv(image_metadata_path),
+                    on='class_id')
+
+
+
+def create_output_path(dst_dir, template, **kwargs):
+    ''' 
+        Parameters
+
+        dst_dir: str
+            destination directory
+
+        template: str
+            template for filename
+
+        **kwargs:  
+            parameters to complete template
+
+
+        Returns
+
+        label_map_path: str
+    '''
+    # ensure existence of destination directory 
+    os.makedirs(dst_dir, exist_ok=True)
+
+    return os.path.join(dst_dir, template.format(**kwargs))
+
+
+
+def add_labels(metadata):
+    ''' add a "label" column to some metadata by ranking frequency
+        of the associated "class_id"
+
+        Parameters
+
+        metadata: pandas.DataFrame
+            required column: class_id
+
+
+        Returns
+
+        labeled_metadata: pandas.DataFrame
+    '''
+    return (metadata.groupby('class_id')
+                    .size()
+                    .rank(method='first', ascending=False)  
+                    .astype(int)
+                    .to_frame(name='label')
+                    .merge(metadata, left_index=True, right_on='class_id'))
+
+
+
+def get_top_metadata(top, metadata):
+    ''' get valid top and top metadata based on label
+
+        Parameters
+
+        top: False or int
+            positive int indicating number of classes to select
+            to select
+            False to indicate that all classes should be selected
+
+        metadata: pandas.DataFrame
+            image/class metadata
+            required columns: class_id, label
+
+
+        Returns
+
+        (valid_top, top_metadata): tuple(int, pandas.DataFrame)
+            valid_top is a positive integer less than the total number of classes
+            top_metadata is the "top" ranked classes as indicated "label" column
+
+    '''
+    nr_classes_occurring = metadata.class_id.unique().size
+    if not top or top > nr_classes_occurring:
+        top = nr_classes_occurring
+
+    return top, metadata.query('label <= @top')

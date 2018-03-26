e15ac3d176cdb0a2fc89cf26bf93c5de60dfb4ae
octaexon <octaexon@gmail.com>
2018-03-26 11:04:53 +0200

    Remove unused utility functions

--- utilities/metadata_utilities.py
+++ utilities/metadata_utilities.py
@@ -16,27 +16,6 @@ __email__ = 'octaexon@gmail.com'
 __status__ = 'Development'
 
 
-def load_metadata(class_metadata_path, image_metadata_path):
-    ''' metadata loader
-
-        Parameters
-
-        class_metadata_path: str
-            required column: class_id
-
-        image_metadata_path: str
-            required column: class_id
-
-
-        Returns
-
-        merged metadata: pandas.DataFrame
-    '''
-    return pd.merge(pd.read_csv(class_metadata_path), 
-                    pd.read_csv(image_metadata_path),
-                    on='class_id')
-
-
 
 def read_image_properties(properties_path):
     ''' reads image properties file if it exists
@@ -54,31 +33,6 @@ def read_image_properties(properties_path):
     if os.path.exists(properties_path):
         return pd.read_csv(properties_path)
     return None
-
-
-
-def create_output_path(dst_dir, template, **kwargs):
-    ''' 
-        Parameters
-
-        dst_dir: str
-            destination directory
-
-        template: str
-            template for filename
-
-        **kwargs:  
-            parameters to complete template
-
-
-        Returns
-
-        label_map_path: str
-    '''
-    # ensure existence of destination directory 
-    os.makedirs(dst_dir, exist_ok=True)
-
-    return os.path.join(dst_dir, template.format(**kwargs))
 
 
 
@@ -112,22 +66,3 @@ def select_metadata(metadata, top):
                     .to_frame(name='id')
                     .query('id <= @top')
                     .merge(metadata, left_index=True, right_on='class_id'))
-
-
-
-def get_metadata_for_ids(metadata):
-    ''' returns metadata that has been given label map ids
-
-        Parameter
-
-        metadata: pandas.DataFrame
-            image metadata
-            required column: id
-
-
-        Return
-
-        pandas.DataFrame
-            image metadata with non-null ids
-    '''
-    return metadata[pd.notnull(metadata.id)]
